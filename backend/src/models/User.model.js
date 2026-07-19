04aca471e3f56e12181f2d9b1003dbc66460984b
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-19 15:42:57 +0530

    feat: add friends field to User model schema

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -39,6 +39,12 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     timestamps: true,
