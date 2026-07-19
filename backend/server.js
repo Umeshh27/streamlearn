371d81ae942c1d87638dfa57a24ba018a6d78ef7
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-19 10:41:34 +0530

    fix: correct formatting and improve API route responses in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
-import express from "express"
+import express from "express";
 
-const app=express()
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("server is running")
-})
+app.get("/api/auth/signup", (req, res) => {
+  res.send("Signup Route");
+});
 
-app.listen(5000,()=>{
-    console.log("server is running on port 5000")
+app.get("/api/auth/login", (req, res) => {
+  res.send("Login Route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+  res.send("Logout Route");
+});
+
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
 });
