f205bb9484021e72ece88bcdde52c355ff5a4b3d
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-19 15:40:06 +0530

    fix: correct formatting and structure of userSchema in User model

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,27 +1,46 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  FullName: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    FullName: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    inOnBorded: {
+      type: Boolean,
+      default: false,
+    },
   },
-  Email: {
-    type: String,
-    required: true
+  {
+    timestamps: true,
   },
-  Password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  bio: {
-    type: String,
-    default: ""
-  },
-  profilePic: {
-    type: String,
-    default: ""
-  }
-},{
-  timestamps: true,
-});
+);
