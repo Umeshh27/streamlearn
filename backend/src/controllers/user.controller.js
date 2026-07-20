6f07c4fc0fffcf7c4e7376354f8bff37afe4f8cd
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-20 21:47:40 +0530

    feat: implement friend request functionality and user recommendation logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,39 @@
-export async function getRecommendedUsers(req, res) {}
+import User from "../models/User.model.js";
 
-export async function getMyFriends(req, res) {}
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user._id;
+    const currentUser = req.user;
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, // Exclude the current user
+        { $id: { $nin: currentUser.friends } }, // Exclude users who are already friends
+        { isOnBoarded: true }, // Only include users who are onboarded
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error fetching recommended users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user._id)
+      .select("friends")
+      .populate(
+        "friends",
+        "FullName profilePic nativeLanguage learningLanguage location",
+      );
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.error("Error fetching friends:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
+export async function sendFriendRequest(req, res) {
+  
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUsers, getMyFriends, sendFriendRequest } from "../controllers/user.controller.js";
+im
 
 const router = express.Router();
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
 
 
 
