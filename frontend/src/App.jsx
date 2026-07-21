5f3c67ab32ba67029c04ab1989158dbddc399dfb
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-21 21:38:58 +0530

    feat: add routing and pages for Home, Login, Sign Up, Onboarding, Chat, Call, and Notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import NotificationPage from './pages/NotificationPage.jsx'
 
 const App = () => {
   return (
     <div className='h-screen' data-theme="night">
-      
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/onboarding' element={<OnboardingPage />} />
+        <Route path='/chat' element={<ChatPage />} />
+        <Route path='/call' element={<CallPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+      </Routes>
     </div>
   )
 }
