c4412dace4f2f38bf8af476354bddf993b69d71f
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-21 22:53:17 +0530

    feat: add axios for HTTP requests and implement data fetching with react-query in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,22 @@ import ChatPage from './pages/ChatPage.jsx'
 import CallPage from './pages/CallPage.jsx'
 import NotificationPage from './pages/NotificationPage.jsx'
 import { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
 const App = () => {
+
+  //tanstack query is used for data fetching and caching in react applications. It provides a simple and efficient way to manage server state, handle caching, and perform background updates. It helps to reduce the amount of boilerplate code needed for data fetching and provides a better user experience by keeping the UI in sync with the server state.
+
+  const {data,isLoading,error}= useQuery({
+    queryKey:"todos",
+    queryFn: async()=>{
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+      return res.data;
+    }
+  })
+  console.log(data,isLoading,error);
+
   return (
     <div className='h-screen' data-theme="night">
       <Routes>
