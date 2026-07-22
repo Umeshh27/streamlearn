269001bc6ccd196234a8884ca7dd95b48c092239
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-22 20:06:40 +0530

    feat: add lucide-react for icons and enhance SignUpPage layout and form

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { ShipWheelIcon } from "lucide-react";
 
-function SignUpPage() {
+const SignUpPage = () => {
+  const [signUpData, setSignUpData] = React.useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    
+  };
+
   return (
-    <div>SignUpPage</div>
-  )
-}
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+      {/* // Left side  */}
+      <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* logo */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+            StreamLearn </span>
+          </div>
 
-export default SignUpPage
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p>Join StreamLearn And Start Your Learning Adventure!</p>
+                </div>
+
+                <div className="space-y-3">
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+
+                    <input
+                      type="text"
+                      placeholder="Enter your full name"
+                      className="input input-bordered w-full"
+                      value={signUpData.fullName}
+                      onChange={(e) =>
+                        setSignUpData({ ...signUpData, fullName: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
