b20f62c24d39f9c1735a7d8bcb59af1d68fadafa
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-07-26 17:09:41 +0530

    feat: implement login functionality with custom hook and enhance LoginPage UI

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,6 +5,11 @@ export const signup = async (signUpData) => {
     return res.data;
 }
 
+export const login = async (loginData) => {
+    const res = await axiosInstance.post("/auth/login", loginData);
+    return res.data;
+}
+
 export const getAuthUser = async () => {
       try {
         const res = await axiosInstance.get("/auth/me");
