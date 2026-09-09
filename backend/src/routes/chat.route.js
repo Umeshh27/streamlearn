485f1f1f25e61c3ad5a53d36cc9ffc2fae7f15bf
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-09-09 08:54:30 +0530

    feat(backend): update chat routes

--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getStreamToken } from "../controllers/chat.controller.js";
+import { getStreamToken, clearChatHistory, getOrCreateChatChannel } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
 router.get("/token", protectRoute, getStreamToken);
+router.post("/channel/:targetUserId", protectRoute, getOrCreateChatChannel);
+router.delete("/clear/:targetUserId", protectRoute, clearChatHistory);
 
 export default router;
+
