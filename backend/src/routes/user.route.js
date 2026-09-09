428c2bdd60ce9a4045179057bf9c9cc4e69282cf
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-09-09 08:54:35 +0530

    feat(backend): update user management routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,11 +2,19 @@ import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   acceptFriendRequest,
+  rejectFriendRequest,
+  dismissNotification,
+  clearAllNotifications,
   getFriendRequests,
+  getAllUsers,
   getMyFriends,
   getOutgoingFriendReqs,
   getRecommendedUsers,
+  getUserProfile,
   sendFriendRequest,
+  unfriendUser,
+  updateUserProfile,
+  respondToStaffInvitation,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -15,12 +23,22 @@ const router = express.Router();
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
+router.get("/all-users", getAllUsers);
 router.get("/friends", getMyFriends);
+
+router.get("/profile/:id", getUserProfile);
+router.put("/profile", updateUserProfile);
 
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
+router.put("/friend-request/:id/reject", rejectFriendRequest);
+router.delete("/friend-request/:id/dismiss", dismissNotification);
+router.delete("/notifications/clear-all", clearAllNotifications);
+router.delete("/friend/:id", unfriendUser);
 
 router.get("/friend-requests", getFriendRequests);
 router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
+router.post("/staff-invitation/respond", respondToStaffInvitation);
 
 export default router;
